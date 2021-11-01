c311b919396385dc0d515ad0491b37ede671972e
athul <absathulraj@gmail.com>
2021-11-01 10:57:54 +0530

    react-app

--- app.js
+++
@@ -1,7 +0,0 @@
-const container = document.getElementById("app") 
- 
-const content = React.createElement('div', {}, [
-   React.createElement('h1', {}, "I'am a heading"),
-   React.createElement('h3',{},"I'am a heading 2 ") 
-]) 
-ReactDOM.render(content, container)
---
+++ myapp/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() { 
+  return (
+    <div className="App"> 
+    Lorem ipsum dolor sit amet, consectetur  
+     <h4> 
+       I'am h4
+     </h4> 
+     
+        </div>
+  );
+}
+
+export default App;
