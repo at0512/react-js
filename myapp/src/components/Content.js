9f18b868e5a2f8676e1675200dcf6bda60e510a2
athul <absathulraj@gmail.com>
2022-01-18 11:36:53 +0530

    react todo

--- myapp/src/components/Content.js
+++ myapp/src/components/Content.js
@@ -1,47 +1,37 @@
-import Listitem from "./Listitem" 
-import { useState} from 'react'
+import React,{useState} from 'react'
 
-export const Content = () => {  
-    //    let counter = 0 
-    let [counter, setCounter] =  useState(0)
-    //console.log(counter)  
-   // console.log(mysteryFunction)
+const Content = () => { 
+    const [items,setItem] = useState([])  
+    const [value,setValue] = useState('') 
+    const handleSubmit = ()=>{ 
+        console.log(setItem(prev =>[...prev,value]))  
+        setValue('')
+        // console.log(value) 
+       
+    } 
+    const handlInput = (e)=>{
+        setValue(e.target.value)
+    } 
+const handleDelete = (item)=>{
+    console.log(item) 
+    setItem(prev => prev.filter(i=> i != item))
 
+
+}
     return (
-        <div className='Container'>  
-        <h2>{counter}</h2>   
-        <button onClick={()=>{  
-            console.log(counter)
-            counter = counter+1 
-            setCounter(previousvalue=>previousvalue+1
-            )
-        }}
-        >clickme</button>
-   
-        </div>
+        <>
+            <h2>To do app</h2>  
+        
+                <input value={value} onChange={handlInput}type="text " /> 
+                <button onClick={handleSubmit}type='submit'>add</button>
+                
+            <ul>
+                {items.map(item => <li>{item}<button onClick={()=>{
+                    handleDelete(item)
+                }}>Delete</button></li>)}
+            </ul>
+        </>
     )
-} 
-export default Content; 
+}
 
-// import React from 'react' 
-
-// class classComponent extends React.Component{
-//     constructor(){
-//         super(); 
-//         this.state ={
-//             count:0
-//         };
-//     } 
-//     render(){ 
-//         return(
-//         <div className='Container'>   
-//             {/* //     <h2>{counter}</h2>
-//         //     //    <button onClick={()=>{
-//         //     //        counter = counter+1 
-//         //     //    }}> add me</button> */} 
-//         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed dolor dui. Pellentesque imperdiet, lacus vel volutpat tempus, tortor mauris consequat lacus, et congue sem tellus vel tellus. Donec sit amet lobortis lectus. Aliquam hendrerit tortor sit amet pulvinar malesuada. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam a mollis quam, vitae efficitur risus.
-//                 </div> 
-//         )
-//     }
-// }
-// export default classComponent;
+export default Content
